520bc96080029bf0dc79011e91a3e03a8d5c6e0e
Mariane Malheiros <mariane.malheiros@zbra.dev>
2023-03-02 11:59:56 -0300

    added module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ComponentsModule} from "./ui/components/components.module";
+import { ComponentsModule } from './ui/components/components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        ComponentsModule
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ComponentsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

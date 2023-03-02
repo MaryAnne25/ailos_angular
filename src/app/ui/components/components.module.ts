b46efa1d06ef532afea6d1636f12eaad1fc7e9b4
Mariane Malheiros <mariane.malheiros@zbra.dev>
2023-03-02 11:58:21 -0300

    created header component

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    SideMenuComponent
+    SideMenuComponent,
+    HeaderComponent
   ],
   exports: [
-    SideMenuComponent
+    SideMenuComponent,
+    HeaderComponent
   ],
   imports: [
+    MatIconModule
 
   ]
 })
---
+++ src/app/ui/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'ai-header',
+  templateUrl: './header.component.html',
+})
+
+export class HeaderComponent {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer){
+    this.matIconRegistry.addSvgIcon(
+      "notification-bell",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/notification-bell.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "account-circle",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/account-circle.svg")
+    );
+  }
+}

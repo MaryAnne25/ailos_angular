1e3a57b8e2d140702f2d0b1a72caa77ccda43ec3
Mariane Malheiros <mariane.malheiros@zbra.dev>
2023-03-02 07:55:49 -0300

    components module

---
+++ src/app/ui/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+
+@NgModule({
+  declarations: [
+    SideMenuComponent
+  ],
+  exports: [
+    SideMenuComponent
+  ],
+  imports: [
+
+  ]
+})
+export class ComponentsModule {
+
+}

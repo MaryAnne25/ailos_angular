96a4c6fcda0cdffbbe1493471a09fdef43dd1f02
Mariane Malheiros <mariane.malheiros@zbra.dev>
2023-03-02 12:02:05 -0300

    added icons to side menu

--- src/app/ui/components/side-menu/side-menu.component.ts
+++ src/app/ui/components/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'ai-side-menu',
@@ -6,5 +8,14 @@ import {Component} from "@angular/core";
 })
 
 export class SideMenuComponent {
-
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "sandwich-menu",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/menu.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "search",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/search.svg")
+    );
+  }
 }

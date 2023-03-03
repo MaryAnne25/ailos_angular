fb9acdd6b4d084cf66598e00a84bcf326eb65a5b
Mariane Malheiros <mariane.malheiros@zbra.dev>
2023-03-03 08:58:56 -0300

    small changes

--- src/app/ui/components/button/button.component.ts
+++ src/app/ui/components/button/button.component.ts
@@ -5,19 +5,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   templateUrl: './button.component.html'
 })
 export class ButtonComponent implements OnInit {
-  @Input() text: string;
-  @Input() type: string;
-  @Input() block: string;
-  @Output() click = new EventEmitter<Event>();
-  isPrimary: boolean;
-  isSecondary: boolean;
+  @Input() text: string
+  @Input() type: string
+  @Input() block: string
+  @Output() click = new EventEmitter<Event>()
+  isPrimary: boolean
+  isSecondary: boolean
 
   ngOnInit(): void {
-    this.isPrimary = this.type.toLowerCase() == 'primary';
-    this.isSecondary = this.type.toLowerCase() == 'secondary';
+    this.isPrimary = this.type.toLowerCase() == 'primary'
+    this.isSecondary = this.type.toLowerCase() == 'secondary'
   }
 
   clicked(event: Event): void {
-    this.click.emit(event);
+    this.click.emit(event)
   }
 }
